723b473210f4e61b1875d30a5a40510514a7008a
Sabin Dangi <sabindangi@Sabins-Air.lan>
2024-12-26 16:31:56 -0600

    Section 1 - learning Apache Camel and SpringBoot

--- camel-microservice-a/src/main/java/com/learn/microservices/camel_microservice_a/routes/a/MyFirstTimerRouter.java
+++ camel-microservice-a/src/main/java/com/learn/microservices/camel_microservice_a/routes/a/MyFirstTimerRouter.java
@@ -2,11 +2,22 @@ package com.learn.microservices.camel_microservice_a.routes.a;
 
 import java.time.LocalDateTime;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class MyFirstTimerRouter extends RouteBuilder{
+	
+	@Autowired
+	private GetCurrentTimeBean getCurrentTimeBean;
+	
+	@Autowired
+	private SimpleLogggingProcessingComponent loggingComponent;
 
 	@Override
 	public void configure() throws Exception {
@@ -16,10 +27,20 @@ public class MyFirstTimerRouter extends RouteBuilder{
 		//Exchange[ExchangePattern: InOnly, BodyType: null, Body: [Body is null]]
 		
 		from("timer:first-timer") 
-		//.transform().constant("My Constant Message")
+		.log("${body}") //null
+		.transform().constant("My Constant Message")
+		.log("${body}") // My Constant Message
 		//.transform().constant("Time now is "+LocalDateTime.now())
-		.bean("getCurrentTimeBean")
-		.to("log:first-timer");		
+		
+		//Processing 
+		//Transformation
+		
+		.bean(getCurrentTimeBean,"getCurrentTime")
+		.log("${body}") // Time now is 2024-12-26T15:05:37.643983
+		.bean(loggingComponent)
+		.log("${body}")
+		.process(new SimpleLogggingProcesser())
+		.to("log:first-timer");	//database	
 	}
 
 }
@@ -27,6 +48,24 @@ public class MyFirstTimerRouter extends RouteBuilder{
 @Component
 class GetCurrentTimeBean{
 	public String getCurrentTime() {
-		return "Time now is "+LocalDateTime.now();
+		return "Time now is " + LocalDateTime.now();
+	}
+}
+
+@Component
+class SimpleLogggingProcessingComponent{
+	Logger logger = LoggerFactory.getLogger(SimpleLogggingProcessingComponent.class);
+	
+	public void process(String message) {
+		logger.info("SimpleLogggingProcessingComponent {}",message);
+	}
+}	
+	
+class SimpleLogggingProcesser implements Processor{
+	Logger logger = LoggerFactory.getLogger(SimpleLogggingProcesser.class);
+		
+	@Override
+	public void process(Exchange exchange) throws Exception {
+		logger.info("SimpleLogggingProcesser {}",exchange.getMessage().getBody());	
 	}
 }
